85d8cebcb6e7f3c7d5340829596969e025a16ba6
Esraa Mohamed <esraaahmedaliahmed@gmail.com>
2020-06-22 23:10:36 +0200

    Initial Commit of infrastrucre.Data project update MVC for new UnversityDBContext

--- CleanArch/CleanArch.MVC/Startup.cs
+++ CleanArch/CleanArch.MVC/Startup.cs
@@ -12,6 +12,7 @@ using CleanArch.MVC.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CleanArch.Infra.Data.Context;
 
 namespace CleanArch.MVC
 {
@@ -32,6 +33,12 @@ namespace CleanArch.MVC
                     Configuration.GetConnectionString("UniversityIdentityDBConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddDbContext<UniversityDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("UniversityDBConnection"));
+            });
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
